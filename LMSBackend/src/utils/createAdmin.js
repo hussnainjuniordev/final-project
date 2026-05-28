0458dd00286c10ec58507b4cef945a6c23fa474f
hussnainjuniordev <javaidhussnain521@gmail.com>
2026-05-29 00:22:48 +0500

    feat: adding clear path

--- LMSBackend/src/utils/createAdmin.js
+++ LMSBackend/src/utils/createAdmin.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
